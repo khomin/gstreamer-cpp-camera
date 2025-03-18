ba367645fddc62d028a03cc654d187fe2bbd60e5
kernel panic <hominvladimir@gmail.com>
2025-03-18 18:58:12 +0400

    Update main.cpp

--- example/video_file_to_view/main.cpp
+++ example/video_file_to_view/main.cpp
@@ -13,14 +13,63 @@ int main(int argc, char *argv[]) {
 //        launch->imageRight = std::make_shared<ImageProvider>(1920, 1080);
 
         auto samples = std::vector<std::string>{
-//            "/home/khomin/Desktop/test-images/big_buck_bunny.mp4",
-//            "/home/khomin/Desktop/test-images/sample-15s.mp4",
-//            "/home/khomin/Desktop/test-images/demo.mp4"
-//            "/home/khomin/Desktop/test-images/demo.mp4",
-//            "/home/khomin/Desktop/test-images/demo.mp4",
-//            "/home/khomin/Desktop/test-images/demo.mp4"
-            "/home/khomin/Desktop/test-images/sample-videos/driver-action-recognition.mp4"
-//            "/home/khomin/Desktop/test-images/sample-videos/bolt-multi-size-detection.mp4"
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo038.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo055.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo029.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo031.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo023.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo024.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo020.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo006.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo007.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo016.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo035.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo004.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo050.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo043.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo034.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo032.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo021.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo010.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo012.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output021.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo011.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo000.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo018.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo037.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo025.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo002.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo041.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo044.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo033.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo008.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo053.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo028.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo003.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo049.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo027.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo048.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo046.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo030.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo022.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo019.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo001.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo052.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo014.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo013.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo047.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo015.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo005.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo054.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo039.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo009.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo051.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo017.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo042.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo040.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo026.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo036.mp4",
+                "/home/khomin/Desktop/test-images/bot/videos/output_allo045.mp4"
         };
         int samplesIndex = 0;
 
@@ -45,7 +94,7 @@ int main(int argc, char *argv[]) {
             sinkCallback->start();
             srcFile->start();
             // let it run
-            std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
             // reset
             srcFile.reset();
             sinkCallback.reset();
